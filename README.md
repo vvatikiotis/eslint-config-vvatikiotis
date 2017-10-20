147a4c873be2dd072b114d38e3c34cd3a32c5113
Vassilis Vatikiotis <vvatikiotis@gmail.com>
2017-10-20 17:40:58 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Description
+
+My eslint rules.
+
+To create your eslint config package:
+1. Create your eslint-config project just like this one.
+1. When ready, `npm link`.
+1. Install globally all dependencies specified in your eslint-config package.
+1. Create a home `.eslintrc`, extending your globally linked eslint config (from
+previous step). You can always override with a project local `.eslintrc`.
+
+To use your eslint config package in your projects:
+1. `npm link eslint-config-your-config`.
+1. All the config and plugin packages specified in your eslint-config package must
+be linked in your project.
+
+I don't like the mandatory linking stuff. If you know a better way please create
+an issue.
---
+++ index.js
@@ -0,0 +1,36 @@
+const rules = {
+  'semi': ['warn', 'never'],
+  'quotes': 'warn',
+  'max-len': ['warn', 160, 2],
+  'generator-star-spacing': 'warn',
+  'comma-dangle': ['warn', 'always-multiline'],
+  'key-spacing': 'warn',
+  'no-unused-vars': 'warn',
+  'no-use-before-define': 'warn',
+  'object-curly-spacing': ['warn', 'always'],
+  'object-property-newline': 0,
+  'space-before-function-paren': ['warn', 'never'],
+  'eol-last': 'warn',
+  'arrow-spacing': 'warn',
+  'no-trailing-spaces': 'warn',
+  'jsx-quotes': ['warn', 'prefer-single'],
+  'react/jsx-key': 'warn',
+  'react/self-closing-comp': 'warn',
+}
+
+module.exports = {
+  parser: 'babel-eslint',
+  extends: [
+    'standard',
+    'standard-react',
+  ],
+  plugins: [
+    'babel',
+    'react',
+  ],
+  env: {
+    node: true,
+    browser: true,
+  },
+  rules,
+}
